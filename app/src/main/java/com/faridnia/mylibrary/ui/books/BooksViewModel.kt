1e46762966d69a1c69671ec9b549d03281ee5238
Milad <milad.faridnia@gmail.com>
2021-10-26 17:36:06 +0330

    feat: add and config dependency injection with dagger hilt

--- app/src/main/java/com/faridnia/mylibrary/ui/books/BooksViewModel.kt
+++ app/src/main/java/com/faridnia/mylibrary/ui/books/BooksViewModel.kt
@@ -2,8 +2,11 @@ package com.faridnia.mylibrary.ui.books
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.faridnia.mylibrary.data.BooksDao
 
 class BooksViewModel @ViewModelInject constructor(private val dao: BooksDao) : ViewModel() {
+
+    val books = dao.getBooks().asLiveData()
 
 }
